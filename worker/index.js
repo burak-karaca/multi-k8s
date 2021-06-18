0d673c0827ea98d5e8ab998bf428298228728222
Karaca <burak.karaca@nokia.com>
2021-06-18 12:27:20 +0200

    new commit

--- worker/index.js
+++ worker/index.js
@@ -1,18 +1,21 @@
+// required for connecting to redis db
 const keys = require('./keys');
 const redis = require('redis');
 
 const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
+  // if it ever loses connection to redis server, it will try to connect once in 1ms
   retry_strategy: () => 1000
 });
-const sub = redisClient.duplicate();
 
+const sub = redisClient.duplicate();
+// function for calculating Fibonacci values
 function fib(index) {
   if (index < 2) return 1;
   return fib(index - 1) + fib(index - 2);
 }
-
+// any time we get a new value from redis, the new fib value will be calculated and insert to redis instance
 sub.on('message', (channel, message) => {
   redisClient.hset('values', message, fib(parseInt(message)));
 });
--- worker/keys.js
+++ worker/keys.js
@@ -1,3 +1,4 @@
+// any time we want to connect to Redis, we will use these env variable names to connect
 module.exports = {
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT
